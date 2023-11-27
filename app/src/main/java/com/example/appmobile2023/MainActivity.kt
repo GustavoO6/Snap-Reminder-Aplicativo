9bdd13ea88b1de5ac1d85e93cbc846c96a4092ce
GustavoO6 <gustavo36546@gmail.com>
2023-11-27 20:51:04 -0300

    Snap Reminder Aplicativo de Notas.

--- app/src/main/java/com/example/appmobile2023/MainActivity.kt
+++ app/src/main/java/com/example/appmobile2023/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.example.appmobile2023
 
-// MainActivity.kt - Ponto de entrada da aplicação Android
-
 // Importe pacotes necessários
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 
-// Classe de atividade principal
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // Define o layout para a atividade principal
     }
 }
-// Note.kt - Classe de dados representando uma nota
 
-// Classe de dados para armazenar informações da nota
 data class Note(
     val id: Int,    // Identificador único para cada nota
     var title: String?,  // Título da nota
@@ -25,20 +20,16 @@ data class Note(
 ) {
 
 }
-// NoteAdapter.kt - Classe adaptadora para manipular notas em um RecyclerView
 
-// Importe pacotes necessários
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// Classe adaptadora para manipular notas em um RecyclerView
 class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(),
     Parcelable {
 
-    // Classe interna ViewHolder
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
@@ -47,21 +38,18 @@ class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdap
     constructor(parcel: Parcel) : this(parcel.createTypedArrayList(Note)) {
     }
 
-    // Crie novos ViewHolders
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_note, parent, false)
         return NoteViewHolder(itemView)
     }
 
-    // Vincule dados aos ViewHolders
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = notes[position]
         holder.titleTextView.text = currentNote.title
         holder.contentTextView.text = currentNote.content
     }
 
-    // Retorne o tamanho do conjunto de dados
     override fun getItemCount() = notes.size
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeTypedList(notes)
@@ -86,19 +74,16 @@ private fun Parcel.writeTypedList(notes: List<Note>) {
 
 }
 
-// NoteDatabase.kt - Classe singleton para gerenciar o banco de dados de notas
 
-// Importe pacotes necessários
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-// Defina o banco de dados Room
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class NoteDatabase() : RoomDatabase(), Parcelable {
     constructor(parcel: Parcel) : this() {
     }
 
-    abstract fun noteDao(): NoteDao // Método abstrato para acessar o NoteDao
+    abstract fun noteDao(): NoteDao
     override fun writeToParcel(parcel: Parcel, flags: Int) {
 
     }
@@ -117,19 +102,15 @@ abstract class NoteDatabase() : RoomDatabase(), Parcelable {
         }
     }
 }
-// NoteDao.kt - Interface de Objeto de Acesso a Dados para interagir com o banco de dados de notas
 
-// Importe pacotes necessários
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
-// Interface de Objeto de Acesso a Dados para interagir com o banco de dados de notas
 @Dao
 interface NoteDao {
 
-    // Inserir uma nova nota
     @Insert
     suspend fun insert(note: Note)
 
